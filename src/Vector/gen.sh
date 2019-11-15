48e8efa8d0ae281229d498b5dc16037fef3e1486
Qijian Zhang <arch.strange@foxmail.com>
2019-11-15 20:42:36 +0800

    Refine Vector templates and gen.sh script

--- src/Vector/gen.sh
+++ src/Vector/gen.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-echo 'Vector "I8Vector" T "int8_t"' | ohce Vector.h.ohce > I8Vector.h
-echo 'Vector "I8Vector" T "int8_t"' | ohce Vector.c.ohce > I8Vector.c
+for t in h c; do
+	for w in 8 16 32 64; do
+		ohce Vector.${t}.ohce > I${w}Vector.${t} <<EOF
+{
+	Vector "I${w}Vector"
+	T "int${w}_t"
+}
+EOF
+		ohce Vector.${t}.ohce > U${w}Vector.${t} <<EOF
+{
+	Vector "U${w}Vector"
+	T "uint${w}_t"
+}
+EOF
+	done
+done
 
-echo 'Vector "I16Vector" T "int16_t"' | ohce Vector.h.ohce > I16Vector.h
-echo 'Vector "I16Vector" T "int16_t"' | ohce Vector.c.ohce > I16Vector.c
+echo '{Vector "Str" T "uint8_t"}' | ohce Vector.h.ohce > Str.h
+echo '{Vector "Str" T "uint8_t"}' | ohce Vector.c.ohce > Str.c
 
-echo 'Vector "I32Vector" T "int32_t"' | ohce Vector.h.ohce > I32Vector.h
-echo 'Vector "I32Vector" T "int32_t"' | ohce Vector.c.ohce > I32Vector.c
+echo '{Vector "F32Vector" T "float"}' | ohce Vector.h.ohce > F32Vector.h
+echo '{Vector "F32Vector" T "float"}' | ohce Vector.c.ohce > F32Vector.c
 
-echo 'Vector "I64Vector" T "int64_t"' | ohce Vector.h.ohce > I64Vector.h
-echo 'Vector "I64Vector" T "int64_t"' | ohce Vector.c.ohce > I64Vector.c
+echo '{Vector "F64Vector" T "double"}' | ohce Vector.h.ohce > F64Vector.h
+echo '{Vector "F64Vector" T "double"}' | ohce Vector.c.ohce > F64Vector.c
 
-echo 'Vector "Str" T "uint8_t"' | ohce Vector.h.ohce > Str.h
-echo 'Vector "Str" T "uint8_t"' | ohce Vector.c.ohce > Str.c
+for t in h c; do
+	ohce Vector.${t}.ohce > StrVector.${t} <<EOF
+{
+	Vector "StrVector"
+	T "Str"
+	h_include '#include "Str.h"'
+	T_new "Str_new"
+	T_clone "Str_clone"
+	T_copy "Str_copy"
+	T_free "Str_free"
+}
+EOF
+done
 
-echo 'Vector "U16Vector" T "uint16_t"' | ohce Vector.h.ohce > U16Vector.h
-echo 'Vector "U16Vector" T "uint16_t"' | ohce Vector.c.ohce > U16Vector.c
-
-echo 'Vector "U32Vector" T "uint32_t"' | ohce Vector.h.ohce > U32Vector.h
-echo 'Vector "U32Vector" T "uint32_t"' | ohce Vector.c.ohce > U32Vector.c
-
-echo 'Vector "U64Vector" T "uint64_t"' | ohce Vector.h.ohce > U64Vector.h
-echo 'Vector "U64Vector" T "uint64_t"' | ohce Vector.c.ohce > U64Vector.c
-
-echo 'Vector "F32Vector" T "float"' | ohce Vector.h.ohce > F32Vector.h
-echo 'Vector "F32Vector" T "float"' | ohce Vector.c.ohce > F32Vector.c
-
-echo 'Vector "F64Vector" T "double"' | ohce Vector.h.ohce > F64Vector.h
-echo 'Vector "F64Vector" T "double"' | ohce Vector.c.ohce > F64Vector.c
-
-echo 'Vector "PtrVector" T "void *"' | ohce Vector.h.ohce > PtrVector.h
-echo 'Vector "PtrVector" T "void *"' | ohce Vector.c.ohce > PtrVector.c
