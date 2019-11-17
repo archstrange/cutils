86508dcf16ceffb5065a0acd57b390f91641e6bd
Qijian Zhang <arch.strange@foxmail.com>
2019-11-17 17:03:23 +0800

    Refine List ohce template and gen.sh script

--- src/List/gen.sh
+++ src/List/gen.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
-echo 'List "I8List" T "int8_t"' | ohce List.h.ohce > I8List.h
-echo 'List "I8List" T "int8_t"' | ohce List.c.ohce > I8List.c
+for t in h c; do
+	for w in 8 16 32 64; do
+		ohce List.${t}.ohce > I${w}List.${t} <<EOF
+{
+	List "I${w}List"
+	T "int${w}_t"
+}
+EOF
+		ohce List.${t}.ohce > U${w}List.${t} <<EOF
+{
+	List "U${w}List"
+	T "uint${w}_t"
+}
+EOF
+	done
+	ohce List.${t}.ohce > F32List.${t} <<EOF
+{
+	List "F32List"
+	T "float"
+}
+EOF
+	ohce List.${t}.ohce > F64List.${t} <<EOF
+{
+	List "F64List"
+	T "double"
+}
+EOF
+done
 
-echo 'List "I16List" T "int16_t"' | ohce List.h.ohce > I16List.h
-echo 'List "I16List" T "int16_t"' | ohce List.c.ohce > I16List.c
+for t in h c; do
+	ohce List.${t}.ohce > StrList.${t} <<EOF
+{
+	List "StrList"
+	T "Str"
+	h_include '#include "../Vector/Str.h"'
+	T_new "Str_new"
+	T_clone "Str_clone"
+	T_copy "Str_copy"
+	T_free "Str_free"
+}
+EOF
+done
 
-echo 'List "I32List" T "int32_t"' | ohce List.h.ohce > I32List.h
-echo 'List "I32List" T "int32_t"' | ohce List.c.ohce > I32List.c
-
-echo 'List "I64List" T "int64_t"' | ohce List.h.ohce > I64List.h
-echo 'List "I64List" T "int64_t"' | ohce List.c.ohce > I64List.c
-
-echo 'List "U8List" T "uint8_t"' | ohce List.h.ohce > U8List.h
-echo 'List "U8List" T "uint8_t"' | ohce List.c.ohce > U8List.c
-
-echo 'List "U16List" T "uint16_t"' | ohce List.h.ohce > U16List.h
-echo 'List "U16List" T "uint16_t"' | ohce List.c.ohce > U16List.c
-
-echo 'List "U32List" T "uint32_t"' | ohce List.h.ohce > U32List.h
-echo 'List "U32List" T "uint32_t"' | ohce List.c.ohce > U32List.c
-
-echo 'List "U64List" T "uint64_t"' | ohce List.h.ohce > U64List.h
-echo 'List "U64List" T "uint64_t"' | ohce List.c.ohce > U64List.c
-
-echo 'List "F32List" T "float"' | ohce List.h.ohce > F32List.h
-echo 'List "F32List" T "float"' | ohce List.c.ohce > F32List.c
-
-echo 'List "F64List" T "double"' | ohce List.h.ohce > F64List.h
-echo 'List "F64List" T "double"' | ohce List.c.ohce > F64List.c
-
-echo 'List "PtrList" T "void *"' | ohce List.h.ohce > PtrList.h
-echo 'List "PtrList" T "void *"' | ohce List.c.ohce > PtrList.c
